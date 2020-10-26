38bc2ae8bc9ff45eb3cbb520274351d42f6159dc
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2020-10-26 10:13:31 +0100

    Alle Timer-Button funktionieren jetzt

--- func.js
+++ func.js
@@ -3,55 +3,94 @@ const btnShortBreak = document.getElementById("btn-short-break");
 const btnLongBreak = document.getElementById("btn-long-break");
 const btnStart = document.getElementById("btn-start");
 const btnStop = document.getElementById("btn-stop");
-const btnReset = document.getElementById("btn-reset");
 var displayTime = document.getElementById("display-time");
 
 var endTime;
+var currentTime;
+var timeLeft;
+var minutes;
+var seconds;
 
 btnPomodoro.addEventListener("click",function () {
     // Gibt die Zeit an, zu der der Countdown zu Ende sein soll. Hier nach 30 Sekunden.
     // Hier 32, weil es sonst bei 28 Sekunden beginnt zu zählen.
-    endTime = new Date(Date.now() + 1000 * 32);
+    endTime = new Date(Date.now() + 1000 * 302);
     startCountdown(endTime);
-});
-
-btnShortBreak.addEventListener("click", function () {
-    endTime = new Date(Date.now() + 1000 * 300);
-    startCountdown(endTime);
-});
-
-btnLongBreak.addEventListener("click", function() {
-    endTime = new Date(Date.now() + 1000 * 600);
-    startCountdown(endTime);
-});
-
-btnStop.addEventListener("click", function stopCountdown() {
-    clearInterval(countdown);
-    document.getElementById("display-time").innerHTML = "00:00";
 });
 
 function startCountdown(endTime){
     // Updatet die Zeitanzeige jede Sekunde
     var countdown = setInterval(function () {
         // Gibt heute Zeit an
-        var currentTime = new Date().getTime();
+        currentTime = new Date().getTime();
 
         // Berechnet übrige Zeit zwischen Startzeit und jetziger Zeit
-        var timeLeft = endTime - currentTime;
+        timeLeft = endTime - currentTime;
 
         // Berechnet die Minuten und Sekunden
-        var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+        minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
         // Zeigt Minuten und Sekunden im Browser an
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
         displayTime.innerHTML = minutes + ":" + seconds;
 
+        // Hält den Countdown an, wenn "Anhalten" geklickt wird
+        if (btnStop.addEventListener("click", function () {
+            console.log("Minuten: " + parseInt(minutes));
+            console.log("Sekunden: " + seconds);
+            stopCountdown(countdown, minutes, seconds);
+        }));
+
+        if (btnShortBreak.addEventListener("click", function () {
+            clearInterval(countdown);
+            displayTime.innerHTML = "00:00";
+        }));
+
+        if (btnLongBreak.addEventListener("click", function () {
+            clearInterval(countdown);
+            displayTime.innerHTML = "00:00";
+        }));
+
         // Stoppt Intervall, wenn die Zeit um ist
         if (timeLeft < 0) {
             clearInterval(countdown);
-            document.getElementById("display-time").innerHTML = "00:00";
+            displayTime.innerHTML = "00:00";
         } 
-    }, 1000);
+    }, 1000);   
 }
+
+function stopCountdown(countdown, minutes, seconds) {
+    clearInterval(countdown);
+    minutes = parseInt(minutes);
+    seconds = parseInt(seconds);
+    var newSeconds = (minutes * 60 + seconds + 2)
+    console.log(newSeconds);
+    if (btnStart.addEventListener("click", function () {
+        endTime = new Date (Date.now() + 1000 * newSeconds);
+        startCountdown(endTime);
+    }));
+};
+
+
+btnShortBreak.addEventListener("click", function () {
+    // stoppt Countdown und setzt ihn zurück
+    if (btnLongBreak.addEventListener("click", function (countdown) {
+        clearInterval(countdown);
+    }));
+
+    endTime = new Date(Date.now() + 1000 * 302);
+    startCountdown(endTime);
+});
+
+
+btnLongBreak.addEventListener("click", function () {
+    if (btnShortBreak.addEventListener("click", function (countdown) {
+        clearInterval(countdown);
+    }));
+
+    endTime = new Date(Date.now() + 1000 * 602);
+    startCountdown(endTime);
+});
+
